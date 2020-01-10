02053d252739fcb636eefacb5b31f9676ef22bb6
Guillaume <guillaume.bogard@cpexterne.org>
2020-01-10 15:31:53 +0100

    Added raw file source

--- src/main/scala/lambda/domain/code/SourceFile.scala
+++ src/main/scala/lambda/domain/code/SourceFile.scala
@@ -1,7 +1,18 @@
 package lambda.domain.code
 
-sealed trait  SourceFile
+sealed trait SourceFile {
+  def fileName: Option[String]
+}
 
 object SourceFile {
-  case class ClasspathResource(name: String) extends SourceFile
+
+  case class ClasspathResource(name: String) extends SourceFile {
+    def fileName: Option[String] = name.split('/').last.trim match {
+      case "" => None
+      case str => Some(str)
+    }
+  }
+
+  case class RawText(content: String, fileName: Option[String] = None) extends SourceFile
+
 }
