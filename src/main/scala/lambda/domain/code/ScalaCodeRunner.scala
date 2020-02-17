842f677ba3e118d2548e8731d1f6ca8bd45a8307
Guillaume <guillaume.bogard@cpexterne.org>
2020-02-17 17:11:18 +0100

    Updated scala code runner

--- src/main/scala/lambda/domain/code/ScalaCodeRunner.scala
+++ src/main/scala/lambda/domain/code/ScalaCodeRunner.scala
@@ -4,17 +4,12 @@ import java.io.File
 import ScalaCodeRunner._
 
 trait ScalaCodeRunner[F[_]] {
+
   def runFiles(
       files: List[File],
       dependencies: List[ScalaDependency] = Nil,
   ): ProcessResult[F]
 
-  def runCodeString(
-    code: String,
-    wrapInMain: Boolean = true,
-    baseFiles: List[File] = Nil,
-    dependencies: List[ScalaDependency] = Nil,
-  ): ProcessResult[F]
 }
 
 object ScalaCodeRunner {
